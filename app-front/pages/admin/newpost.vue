cbe0d7d2e4ad1b06ba19ae716e758c59f6c3582b
Tatsuya Ichikawa <>
2018-11-25 16:01:50 +0900

    [Add] Add dashboard page

---
+++ app-front/pages/admin/newpost.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <SideBar :links="links"/>
+    <div class="content-wrapper">
+      <AdminHeader />
+      <div>aaaa</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AdminHeader from "~/components/shared/layouts/adminHeader.vue";
+  import SideBar from "~/components/shared/layouts/sideBar.vue";
+
+  export default {
+    components: {
+      SideBar,
+      AdminHeader,
+    },
+    data() {
+      return {
+        links: [
+          { name: "Dashbord", path: "/admin" },
+          { name: "NewPost", path: "/admin/newpost" },
+          { name: "AllPosts", path: "/admin/allposts" },
+        ]
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+  }
+
+  .content-wrapper {
+    width: 100%;
+  }
+</style>
