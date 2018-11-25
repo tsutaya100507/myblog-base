f97a1b58e8a7dc8eae7bd5aa9aec9db6b63f2463
Tatsuya Ichikawa <>
2018-11-26 02:42:13 +0900

    [Add] Add basic form to dashboard

--- app-front/pages/admin/newpost.vue
+++ app-front/pages/admin/newpost.vue
@@ -3,7 +3,36 @@
     <SideBar :links="links"/>
     <div class="content-wrapper">
       <AdminHeader />
-      <div>aaaa</div>
+      <div class="post-form">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="slug"
+            :rules="nameRules"
+            :counter="10"
+            label="Slug"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="title"
+            :rules="nameRules"
+            :counter="10"
+            label="Title"
+            required
+          ></v-text-field>
+          <!-- todo: markdownエディタに書き換え -->
+          <v-text-field
+            v-model="body"
+            :rules="nameRules"
+            label="Body"
+            required
+          ></v-text-field>
+          <v-btn
+            :disabled="!valid"
+            @click="submit"
+          >submit</v-btn>
+          <v-btn @click="clear">clear</v-btn>
+        </v-form>
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +52,39 @@
           { name: "Dashbord", path: "/admin" },
           { name: "NewPost", path: "/admin/newpost" },
           { name: "AllPosts", path: "/admin/allposts" },
-        ]
+        ],
+        valid: true,
+        slug: '',
+        slugRules: [
+          v => !!v || 'Name is required',
+          v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+        ],
+        title: '',
+        titleRules: [
+          v => !!v || 'Name is required',
+          v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+        ],
+        body: '',
+        bodyRules: [
+          v => !!v || 'Name is required',
+          // v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+        ],
+      }
+    },
+    methods: {
+      submit () {
+        if (this.$refs.form.validate()) {
+          // Native form submission is not yet supported
+          axios.post('', {
+            name: this.name,
+            email: this.email,
+            select: this.select,
+            checkbox: this.checkbox
+          })
+        }
+      },
+      clear () {
+        this.$refs.form.reset()
       }
     }
   }
@@ -37,5 +98,11 @@
 
   .content-wrapper {
     width: 100%;
+  }
+
+  .post-form {
+    margin-top: 50px;
+    margin-left: 200px;
+    padding: 20px;
   }
 </style>
