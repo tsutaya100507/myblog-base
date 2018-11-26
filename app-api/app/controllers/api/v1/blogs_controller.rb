d531f7449fd14da24542fc90e2727a75a4798192
Tatsuya Ichikawa <>
2018-11-26 15:37:20 +0900

    [Add] Add blog post api

--- app-api/app/controllers/api/v1/blogs_controller.rb
+++ app-api/app/controllers/api/v1/blogs_controller.rb
@@ -6,6 +6,20 @@ module Api
         # render json: { status: 'SUCCESS', message: 'loaded blogs', data: blogs}, status: :ok
         render json: blogs
       end
+
+      def create
+        blog = Blog.new(blog_params)
+        if blog.save
+          render json: "create new blog.\n", status: 200
+        else
+          render json: "fail to blog.\n", status: 500
+        end          
+      end
+
+      private
+        def blog_params
+          params.require(:blog).permit(:slug, :title, :body)
+        end
     end
   end
 end
